3c3e2f047d22a974c0cf96b8ba0a0cfe4e007ced
JohnnyKleyson <kleysonjohnny2016@gmail.com>
2021-10-26 09:18:50 -0300

    refactor the routes and delete some useless files

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,23 +7,11 @@ import Stats from 'src/components/Dashboard/Views/Dashboard/Stats.vue'
 
 // Pages
 import User from 'src/components/Dashboard/Views/Pages/UserProfile.vue'
-import TimeLine from 'src/components/Dashboard/Views/Pages/TimeLinePage.vue'
 import Login from 'src/components/Dashboard/Views/Pages/Login.vue'
 import Register from 'src/components/Dashboard/Views/Pages/Register.vue'
-import Lock from 'src/components/Dashboard/Views/Pages/Lock.vue'
 import Paciente from 'src/components/Dashboard/Views/Usuarios/Paciente/PacientePage.vue'
 import Profissional from 'src/components/Dashboard/Views/Usuarios/Profissional/ProfissionalPage.vue'
 import ProfissionalProfile from '../components/Dashboard/Views/Usuarios/Profissional/ProfissionalProfile.vue'
-
-
-// Components pages
-import Buttons from 'src/components/Dashboard/Views/Components/Buttons.vue'
-import GridSystem from 'src/components/Dashboard/Views/Components/GridSystem.vue'
-import Panels from 'src/components/Dashboard/Views/Components/Panels.vue'
-import SweetAlert from 'src/components/Dashboard/Views/Components/SweetAlert.vue'
-import Notifications from 'src/components/Dashboard/Views/Components/Notifications.vue'
-import Icons from 'src/components/Dashboard/Views/Components/Icons.vue'
-import Typography from 'src/components/Dashboard/Views/Components/Typography.vue'
 
 // Forms pages
 import RegularForms from 'src/components/Dashboard/Views/Forms/RegularForms.vue'
@@ -35,59 +23,13 @@ import Wizard from 'src/components/Dashboard/Views/Forms/Wizard.vue'
 import RegularTables from 'src/components/Dashboard/Views/Tables/RegularTables.vue'
 import ExtendedTables from 'src/components/Dashboard/Views/Tables/ExtendedTables.vue'
 import PaginatedTables from 'src/components/Dashboard/Views/Tables/PaginatedTables.vue'
-// Maps pages
-import GoogleMaps from 'src/components/Dashboard/Views/Maps/GoogleMaps.vue'
-import FullScreenMap from 'src/components/Dashboard/Views/Maps/FullScreenMap.vue'
-import VectorMaps from 'src/components/Dashboard/Views/Maps/VectorMapsPage.vue'
 
 // Calendar
 import Calendar from 'src/components/Dashboard/Views/Calendar/CalendarRoute.vue'
 // Charts
 import Charts from 'src/components/Dashboard/Views/Charts.vue'
 
-let componentsMenu = {
-  path: '/components',
-  component: DashboardLayout,
-  redirect: '/components/buttons',
-  children: [
-    {
-      path: 'buttons',
-      name: 'Buttons',
-      component: Buttons
-    },
-    {
-      path: 'grid-system',
-      name: 'Grid System',
-      component: GridSystem
-    },
-    {
-      path: 'panels',
-      name: 'Panels',
-      component: Panels
-    },
-    {
-      path: 'sweet-alert',
-      name: 'Sweet Alert',
-      component: SweetAlert
-    },
-    {
-      path: 'notifications',
-      name: 'Notifications',
-      component: Notifications
-    },
-    {
-      path: 'icons',
-      name: 'Icons',
-      component: Icons
-    },
-    {
-      path: 'typography',
-      name: 'Typography',
-      component: Typography
-    }
 
-  ]
-}
 let formsMenu = {
   path: '/forms',
   component: DashboardLayout,
@@ -138,29 +80,6 @@ let tablesMenu = {
     }]
 }
 
-let mapsMenu = {
-  path: '/maps',
-  component: DashboardLayout,
-  redirect: '/maps/google',
-  children: [
-    {
-      path: 'google',
-      name: 'Google Maps',
-      component: GoogleMaps
-    },
-    {
-      path: 'full-screen',
-      name: 'Full Screen Map',
-      component: FullScreenMap
-    },
-    {
-      path: 'vector-map',
-      name: 'Vector Map',
-      component: VectorMaps
-    }
-  ]
-}
-
 let pagesMenu = {
   path: '/pages',
   component: DashboardLayout,
@@ -170,11 +89,6 @@ let pagesMenu = {
       path: 'user',
       name: 'User Page',
       component: User
-    },
-    {
-      path: 'timeline',
-      name: 'Timeline Page',
-      component: TimeLine
     }
   ]
 }
@@ -189,12 +103,6 @@ let registerPage = {
   path: '/register',
   name: 'Register',
   component: Register
-}
-
-let lockPage = {
-  path: '/lock',
-  name: 'Lock',
-  component: Lock
 }
 
 let usuariosPage = {
@@ -239,14 +147,11 @@ const routes = [
     ]
   },
   usuariosPage,
-  componentsMenu,
   formsMenu,
   tablesMenu,
-  mapsMenu,
   pagesMenu,
   loginPage,
   registerPage,
-  lockPage,
   {
     path: '/admin',
     component: DashboardLayout,
