252fc59b4ee6dee1448aa20275fcbdce004c33f9
JohnnyKleyson <kleysonjohnny2016@gmail.com>
2021-11-17 17:08:39 -0300

    feat: push notification page

--- src/components/Dashboard/Views/Push-Notifications/PushNotifications.vue
+++ src/components/Dashboard/Views/Push-Notifications/PushNotifications.vue
@@ -1,14 +1,69 @@
 <template>
-  <div>
-    oi
+<div>
+  <div class="register_button">
+  <router-link to="/notifications/create"><button class="text-uppercase myBtn">Criar Notificação</button></router-link>
   </div>
+
+  <div class="row">
+    <div class="col-lg-12 col-md-12">
+      <paginated-tables
+        tableName="Lista de Notificações"
+        @delete-row="deleteUser"
+        :tableData="users"
+        :propsToSearch="propsToSearch"
+        :tableColumns="tableColumns"
+      />
+    </div>
+    <div class="col-lg-12 col-md-12">
+      <complete-register-card
+        :registerWarning="true"
+      />
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
+import PaginatedTables from '../Tables/PaginatedTables.vue'
 export default {
+  components: {
+    PaginatedTables
+  },
+  data () {
+    return {
+      users: [],
+      propsToSearch: ['addressee', 'message'],
+      tableColumns: [
+        {
+          prop: 'addressee',
+          label: 'DESTINATÁRIO',
+          minWidth: 220
+        },
+        {
+          prop: 'message',
+          label: 'MENSAGEM',
+          minWidth: 300
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.register_button{
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+}
 
+.myBtn{
+  font-weight: 700;
+  padding: 15px 35px;
+  background-color: #718EFA;
+  color:#fff;
+  border-radius: 3px;
+  border: none;
+  letter-spacing: 0.5px;
+}
 </style>
