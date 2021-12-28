40808d091e30e66d7ffd42c9877c86f5a3cb194b
JohnnyKleyson <kleysonjohnny2016@gmail.com>
2021-12-28 11:20:07 -0300

    feat: add user image and delete btn

--- src/components/Dashboard/Views/Usuarios/Paciente/PacienteProfile.vue
+++ src/components/Dashboard/Views/Usuarios/Paciente/PacienteProfile.vue
@@ -1,49 +1,48 @@
 <template>
   <div class="row">
-   <div class="col-lg-4 col-md-5">
-      <user-card
-        :user="user"
-      />
-      <documents-card  
+    <div class="col-lg-4 col-md-5">
+      <user-card :user="user" />
+      <documents-card
         cardName="Validação da carteirinha"
         :cardDocs="true"
         :documentLink="documentLink"
       />
-      <delete-profile-button class="deleteBtn" 
-        @click.native="deletePatientProfile"
-      />
     </div>
 
-    <div class="col-lg-4 col-sm-6"  v-for="stats in statsCards" :key="stats.id">
-        <stats-card>
-          <div class="numbers" slot="content">
-            <p>{{stats.title}}</p>
-               {{stats.value}}
-          </div>
-          <div class="stats" slot="footer">
-            {{stats.footerText}}
-          </div>
-        </stats-card>
+    <div class="col-lg-4 col-sm-6" v-for="(stats, index) in statsCards" :key="index">
+      <stats-card>
+        <div class="numbers" slot="content">
+          <p>{{ stats.title }}</p>
+          {{ stats.value }}
+        </div>
+        <div class="stats" slot="footer">
+          {{ stats.footerText }}
+        </div>
+      </stats-card>
     </div>
-    
+
     <div class="col-lg-8 col-md-7">
-      <PacienteForm 
-        :form="form" 
-        :dependents="dependents"
+      <PacienteForm :form="form" :dependents="dependents" />
+    </div>
+
+    <div class="col-lg-12">
+      <delete-profile-button
+        class="deleteBtn"
+        @click.native="deletePatientProfile"
       />
     </div>
   </div>
 </template>
 
 <script>
-import PatientService from 'src/services/patient.service.js'
-import DocumentsCard from '../../../../UIComponents/Cards/DocumentsCard.vue'
-import PacienteForm from 'src/components/UIComponents/PacienteForm.vue'
-import UserCard from '../../../../UIComponents/UserCard.vue'
-import StatsCard from 'src/components/UIComponents/Cards/StatsCard.vue'
-import Swal from 'sweetalert2'
-import 'sweetalert2/dist/sweetalert2.css'
-import DeleteProfileButton from 'src/components/UIComponents/DeleteProfileButton.vue'
+import PatientService from "src/services/patient.service.js";
+import DocumentsCard from "../../../../UIComponents/Cards/DocumentsCard.vue";
+import PacienteForm from "src/components/UIComponents/PacienteForm.vue";
+import UserCard from "../../../../UIComponents/UserCard.vue";
+import StatsCard from "src/components/UIComponents/Cards/StatsCard.vue";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
+import DeleteProfileButton from "src/components/UIComponents/DeleteProfileButton.vue";
 export default {
   components: {
     UserCard,
@@ -52,86 +51,81 @@ export default {
     DeleteProfileButton,
     PacienteForm
   },
-  data () {
+  data() {
     return {
       user: {},
       form: {},
       dependents: [],
-      documentLink: '',
+      documentLink: "",
       statsCards: [
         {
-          title: 'Atendimentos Realizados',
+          title: "Atendimentos Realizados",
           value: 0,
-          footerText: 'Todos',
-          id: 1
+          footerText: "Todos"
         },
         {
-          title: 'Avaliação',
+          title: "Avaliação",
           value: 0,
-          footerText: 'Referente a todos os atendimentos',
-          id: 2
+          footerText: "Referente a todos os atendimentos"
         }
       ]
-    }
+    };
   },
-  methods:{
-    deletePatientProfile () {
-      const id = this.$route.params.id
+  methods: {
+    deletePatientProfile() {
+      const id = this.$route.params.id;
       Swal({
-        title: 'Excluir cadastro',
-        text: 'Deseja mesmo excluir este perfil?',
-        icon: 'warning',
+        title: "Excluir cadastro",
+        text: "Deseja mesmo excluir este perfil?",
+        icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: '#19B128',
-        cancelButtonColor: '#EF0028',
-        confirmButtonText: 'SIM, EXCLUIR',
-        cancelButtonText: 'CANCELAR'
+        confirmButtonColor: "#19B128",
+        cancelButtonColor: "#EF0028",
+        confirmButtonText: "SIM, EXCLUIR",
+        cancelButtonText: "CANCELAR"
       }).then(() => {
-        Swal(
-          'Sucesso!',
-          'Cadastro excluido com sucesso',
-          'success'
-        )
+        Swal("Sucesso!", "Cadastro excluido com sucesso", "success");
         PatientService.deletePatient(id)
-        .then(()=>{
-          this.$router.push('/usuarios/profissional')
-        })
-      })
+        .then(() => {
+          this.$router.push("/usuarios/profissional");
+        });
+      });
     }
   },
-  async mounted () {
-    const id = this.$route.params.id
+  async mounted() {
+    const id = this.$route.params.id;
     if (id) {
-      PatientService.getPatient(id)
-        .then((result) => {
-          const userData = result.data
-          this.statsCards[0].value = userData.attendance
-          this.statsCards[1].value = userData.rating
-          this.documentLink = userData.elKadriImage
-          this.user = {
-            name: userData.name,
-            email: userData.email,
-            birthDate: userData.birthDate,
-            image: userData.image
-          }
-          this.form = {
-            email: userData.email,
-            cpf: userData.cpf,
-            fullName: userData.name,
-            birthDate: userData.birthDate
-          }
-          for (let i = 0; i < userData.dependents.length; i++) {
-            this.dependents = [{
+      PatientService.getPatient(id).then(result => {
+        console.log(result);
+        const userData = result.data;
+        this.statsCards[0].value = userData.attendance;
+        this.statsCards[1].value = userData.rating;
+        this.documentLink = userData.elKadriImage;
+        this.user = {
+          name: userData.name,
+          email: userData.email,
+          birthDate: userData.birthDate,
+          image: userData.userImage,
+         /*  status: userData.status */
+        };
+        this.form = {
+          email: userData.email,
+          cpf: userData.cpf,
+          fullName: userData.name,
+          birthDate: userData.birthDate
+        };
+        for (let i = 0; i < userData.dependents.length; i++) {
+          this.dependents = [
+            {
               name: userData.dependents[i].name,
-              birthDate: userData.dependents[i].birthDate,
-            }]
-          }
-      })
+              birthDate: userData.dependents[i].birthDate
+            }
+          ];
+        }
+      });
     }
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
