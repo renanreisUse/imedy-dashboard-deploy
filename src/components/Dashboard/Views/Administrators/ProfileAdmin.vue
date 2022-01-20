3f1bf5ff7f8269ae65d604aa9d83284912636478
JohnnyKleyson <kleysonjohnny2016@gmail.com>
2022-01-20 14:32:13 -0300

    feat: delete btn and access level in profile admin

--- src/components/Dashboard/Views/Administrators/ProfileAdmin.vue
+++ src/components/Dashboard/Views/Administrators/ProfileAdmin.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="row">
-    <div class="col-lg-4 col-md-5">
-      <user-card 
-        :user="user" 
-        @account-switch="tester"
+    <div class="col-lg-4 col-md-5 userCard">
+      <user-card
+        :user="user"
+        :showBirthdate="false"
+        @account-switch="switchStatus"
       />
     </div>
 
     <div class="col-lg-8 col-md-7">
       <div class="card">
         <div class="card-header">
-          <h3 class="title">Editar Perfil</h3>
+          <h3 class="title">Perfil</h3>
         </div>
         <div class="card-content">
           <div class="row">
-            <div class="col-md-8">
+            <div class="col-lg-12">
               <fg-input
                 type="email"
                 label="E-mail de acesso"
@@ -24,23 +25,14 @@
               >
               </fg-input>
             </div>
-            <div class="col-md-4">
-              <fg-input
-                type="password"
-                label="Senha de Acesso"
-                :disabled="true"
-                placeholder="Senha de acesso"
-                v-model="password"
-              >
-              </fg-input>
-            </div>
           </div>
 
-          <div class="row">
+          <div class="row inputs">
             <div class="col-md-6">
               <fg-input
                 type="text"
                 label="Nome"
+                :disabled="true"
                 placeholder="Nome"
                 v-model="name"
               >
@@ -50,136 +42,107 @@
               <fg-input
                 type="text"
                 label="Sobrenome"
+                :disabled="true"
                 placeholder="Sobrenome"
                 v-model="lastName"
               >
               </fg-input>
             </div>
           </div>
-          <div class="saveEditBtn">
-            <button @click="changeName" class="imedy-btn text-uppercase">
-              Salvar
-            </button>
-          </div>
         </div>
       </div>
-
-      <div class="col-lg-13">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="title">Alterar senha de acesso</h3>
-            <p class="subtitle">
-              Insira seu e-mail de acesso Imedy para poder alterar sua senha.
-            </p>
-          </div>
-          <div class="card-content">
-            <div class="change-password">
-              <fg-input
-                type="email"
-                class="alteredEmail"
-                label="E-mail de acesso"
-                placeholder="email@exemplo.com.br"
-                v-model="alteredEmail"
-              >
-              </fg-input>
-              <button @click="changePassword" class="imedy-btn text-uppercase">
-                Enviar
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+    <div class="col-lg-12 col-md-12 deleteBtn" v-if="isManager">
+      <delete-profile-button
+        class="deleteBtn"
+        @click.native="deleteAdminProfile"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import UserCard from "../../../UIComponents/UserCard.vue";
-import AuthService from "src/services/auth.service.js";
+import DeleteProfileButton from "src/components/UIComponents/DeleteProfileButton.vue";
 import UserService from "src/services/user.service.js";
-import Swal from "sweetalert2";
-import "sweetalert2/dist/sweetalert2.css";
 export default {
   components: {
-    UserCard
+    UserCard,
+    DeleteProfileButton
   },
   data() {
     return {
       user: {},
       email: "",
-      password: "",
       name: "",
       lastName: "",
-      alteredEmail: ""
+      isManager: false
     };
   },
   methods: {
-    tester(status){
+    switchStatus(status) {
       const data = {
         id: this.$route.params.id,
         status: status
-      }
-      UserService.updateAdminStatus(data)
-      .then((res)=>{
-        console.log(res);
-      })
+      };
+      UserService.updateAdminStatus(data).catch(() => {
+        this.$notify({
+          component: {
+            template: `ERRO - <span>Ops, algo deu errado! Não foi possivél atualizar status.</span>`
+          },
+          icon: "",
+          horizontalAlign: "right",
+          verticalAlign: "top",
+          type: "warning"
+        });
+      });
     },
     getAdminInfo() {
       const id = this.$route.params.id;
       UserService.getAdmin(id).then(({ data }) => {
-        const fullName = data.name;
         console.log(data);
+        const fullName = data.name;
         const name = fullName.split(" ");
         this.name = name[0];
         this.lastName = name[1];
-        this.email = "kleysonjohnny2016@gmail.com";
-        this.password = "123456";
+        this.email = data.email;
         this.user = {
           name: data.name,
-          email: "kleysonjohnny2016@gmail.com",
+          email: data.email,
           image: data.image,
           status: data.status
         };
       });
     },
-    changePassword() {
-      const email = {
-        email: this.alteredEmail
-      };
-      AuthService.recoveryPassword(email).then(res => {
-        console.log(res);
+    deleteAdminProfile() {
+      const id = this.$route.params.id;
+      Swal({
+        title: "Excluir cadastro",
+        text: "Deseja mesmo excluir este perfil?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#19B128",
+        cancelButtonColor: "#EF0028",
+        confirmButtonText: "SIM, EXCLUIR",
+        cancelButtonText: "CANCELAR"
+      }).then(() => {
+        Swal("Sucesso!", "Cadastro excluido com sucesso", "success");
+        UserService.deleteAdmin(id)
+          .then(() => {
+            this.$router.push("/admin/list");
+          })
+          .catch(() =>
+            Swal("Ops!", "Ocorreu um erro ao excluir este perfil.", "warning")
+          );
       });
-    },
-    changeName() {
-      const data = {
-        id: this.$route.params.id,
-        name: `${this.name} ${this.lastName}`
-      };
-      UserService.updateAdminName(data)
-        .then(() => {
-          Swal({
-            type: "success",
-            title: "Sucesso!",
-            text: "Nome atualizado com sucesso.",
-            confirmButtonColor: "#19B128",
-            confirmButtonText: "OK"
-          });
-          this.$router.push("/admin/list");
-        })
-        .catch(() => {
-          Swal({
-            type: "warning",
-            title: "Ops, algo deu errado",
-            text: "Não foi possivel efetuar essa alteração.",
-            confirmButtonColor: "#EF0028",
-            confirmButtonText: "OK"
-          });
-        });
     }
   },
   mounted() {
     this.getAdminInfo();
-    this.alteredEmail = this.email;
+    const data = JSON.parse(localStorage.getItem("user"));
+    if (data.roles[0] === "MANAGER") {
+      this.isManager = true;
+    }
   }
 };
 </script>
@@ -197,5 +160,11 @@ export default {
 }
 .form-group.alteredEmail {
   width: 60%;
+}
+.inputs {
+  margin-bottom: 9.5rem;
+}
+.deleteBtn {
+  margin-top: 45px;
 }
 </style>
